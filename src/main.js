6715a19f527d94f5f3d89742441e8dfc14ccf0db
jc1049520 <jc49520@ly.com>
2019-02-27 19:06:34 +0800

    配置Vconsole

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+if(process.env.consoleFlag){
+  var vconsole = require('./utils/vsconsole')
+}
+console.log('4444',process.env.consoleFlag)
+console.log('555',Vconsole)
 
 Vue.config.productionTip = false
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
